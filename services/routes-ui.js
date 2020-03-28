dfc8e707965daebe816d1d0b92bed91af92f87ec
Max Rule <maxrule@iMac.local>
2020-03-28 09:56:38 +0000

    SEO, other juicy stuff

--- services/routes-ui.js
+++ services/routes-ui.js
@@ -5,6 +5,11 @@ const routes = app => {
 
     app.use('/', express.static('dist/index.html'));
     app.use('/contact-success', express.static('dist/contact-success.html'));
+    app.use('/error-404', express.static('dist/404.html'));
+    app.get('*', function(req, res){
+        res.status(404);
+        res.redirect('/error-404');
+      });
 };
 
 module.exports = routes;
